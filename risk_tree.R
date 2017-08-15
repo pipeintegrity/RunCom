0f289fbbe0ff6105a9f0c5e4ce81635f0113d826
Joel Anderson <pipeintegrity@outlook.com>
2017-08-14 19:52:44 -0500

    added subset code for risksub

--- risk_tree.R
+++ risk_tree.R
@@ -3,3 +3,6 @@ library(dplyr)
 library(partykit)
 
 ###Need to import risk_fix.csv as "risk" this has the numeric fixes for dollars and stationing
+
+#subset risk for just Pof fields
+risksub <- risk[,9:18]
