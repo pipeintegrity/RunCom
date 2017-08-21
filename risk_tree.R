c61b4a54e027a6927a6b0e6eb3c87f91fab36254
Joel Anderson <pipeintegrity@outlook.com>
2017-08-20 23:08:53 -0500

    Moved text around and added Titanic decision tree

--- risk_tree.R
+++ risk_tree.R
@@ -20,3 +20,10 @@ training <- risksub[risk_train,]
 system.time(risk_rf5 <- train(PoF~.,training, method="rf", importance=T, nodesize=500,ntree=500))
 
 plot(varImp(risk_rf5), main='Risk Variable Importance (100K Sample)')
+
+#titanic risk tree
+titan <- read.csv("titanic.csv")
+tree2 <- rpart(survived~age+sex+pclass, data=titan, cp=.02)
+
+
+
